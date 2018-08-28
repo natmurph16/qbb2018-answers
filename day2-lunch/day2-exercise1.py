bc72419cc23034e0e955670333b11bef38877167
Natalie Murphy <nat.murph16@gmail.com>
2018-08-28 14:39:37 -0400

    Scripts and outputs for exercises 1 to 4

---
+++ day2-lunch/day2-exercise1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) > 1:
+    f = open( sys.argv[1] )
+else:
+    f = sys.stdin
+        
+count = 0
+for i, line in enumerate( f ):
+    if line[0] == "@":
+        continue
+    count += 1
+    
+print(count)
+    
